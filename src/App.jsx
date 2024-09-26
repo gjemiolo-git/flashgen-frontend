31d9bc5c37b570a577dd9ad279378561fdce6dc3
gjemiolo-git <=>
2024-09-26 15:28:40 +0100

    Reorganise structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-success'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App;
