8ca6a7ed66192ebeb2ee28cbcace7370e4fb71a6
gjemiolo-git <=>
2024-10-03 14:51:17 +0100

    Add basic axios

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,24 @@
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
+export async function onRegistration(registrationData) {
+    return await axios.post(
+        `{REACT_APP_BACKEND_IP}:{REACT_APP_BACKEND_PORT}/api/register`,
+        registrationData
+    )
+}
+
+export async function onLogin(loginData) {
+    return await axios.post(
+        `{REACT_APP_BACKEND_IP}:{REACT_APP_BACKEND_PORT}/api/register`,
+        loginData
+    )
+}
+
+export async function onLogout() {
+    return await axios.get(`{REACT_APP_BACKEND_IP}:{REACT_APP_BACKEND_PORT}/api/logout`);
+}
+
+export async function fetchProtectedInfo() {
+    return await axios.get(`{REACT_APP_BACKEND_IP}:{REACT_APP_BACKEND_PORT}/api/protected`);
+}
